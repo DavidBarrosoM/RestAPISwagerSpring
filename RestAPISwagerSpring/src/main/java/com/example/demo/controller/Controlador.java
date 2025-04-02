a78f1ad959572a41e507f6003f24c709e2dabcb4
a940731 <a940731@10.234.61.50>
2025-04-02 16:04:21 +0200

    Creadas todas las operaciones CRUD para proyect

--- RestAPISwagerSpring/src/main/java/com/example/demo/controller/Controlador.java
+++ RestAPISwagerSpring/src/main/java/com/example/demo/controller/Controlador.java
@@ -1,10 +1,12 @@
 package com.example.demo.controller;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,25 +33,44 @@ import com.example.demo.model.Proyect;
 import com.example.demo.service.Servicio;
 
 @RestController
-//@Tag(name = "Products API")
+@Tag(name = "Products API")
 public class Controlador {
 	@Autowired
 	Servicio servicio;
     @PostMapping("/proyect")
-    public ResponseEntity<Void> createProyect(@RequestBody Proyect proyecto) {
+    public ResponseEntity<?> createProyect(@RequestBody Proyect proyecto) {
         //creation logic
-        return new ResponseEntity<>(HttpStatus.CREATED);
+    	servicio.agregarProyecto(proyecto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(proyecto);
+    	//return ResponseEntity(servicio.agregarProyecto(proyecto),HttpStatus.CREATED);
     }
+    /*
+     * @PostMapping("")
+    public ResponseEntity<?> createPelicula(@RequestBody Pelicula pelicula) {
+        
+        if (repositorio.findByNombre(pelicula.getNombre()).isPresent()) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("NOMBRE YA EXISTENTE");
+        }
+        Pelicula savedPelicula = repositorio.save(pelicula);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedPelicula);
+    }
+     * */
     @GetMapping("/proyect/{id}")
     public ResponseEntity<Proyect> getProyectById(@PathVariable("id") @Parameter(name = "id") Long id) {
         //retrieval logic
         return ResponseEntity.ok(servicio.getById(id));
     }
     @GetMapping("/proyect")
-    public ResponseEntity<ArrayList<Proyect>> getProyectAll() {
+    public ResponseEntity<List<Proyect>> getProyectAll() {
         //retrieval logic
-        return ResponseEntity.ok(servicio.getAll());
+        return ResponseEntity.ok(servicio.getAllProyectos());
     }
+    
+    @DeleteMapping("/proyect/{id}")
+    public ResponseEntity<?> borraProyectById(@PathVariable("id") @Parameter(name="id") Long id){
+    	return ResponseEntity.status(HttpStatus.ACCEPTED).body("Borrado");
+    }
+    
     /*@Operation(summary = "Devuelve un proyecto por id", description = "Devuelve un proyecto por id")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successfully retrieved"), 
--- RestAPISwagerSpring/src/main/java/com/example/demo/service/Servicio.java
+++ RestAPISwagerSpring/src/main/java/com/example/demo/service/Servicio.java
@@ -6,20 +6,32 @@ import org.springframework.stereotype.Service;
 import com.example.demo.Repositories.RepositorioProyect;
 import com.example.demo.model.Proyect;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Optional;
 @Service
 public class Servicio {
-	@Autowired
-	RepositorioProyect repProyect;
-	public ArrayList<Proyect> getAll(){
-		return (ArrayList<Proyect>) repProyect.findAll();
-	}
+	//@Autowired
+	//RepositorioProyect repProyect;
+	HashMap<Long,Proyect> proyectos = new HashMap<>();
+	public List<Proyect> getAllProyectos() {
+        return new ArrayList<>(proyectos.values());
+    }
 	public Proyect getById(Long id) {
-		Optional<Proyect> p = repProyect.findById(id);
+		return proyectos.get(id);
+		/*
+		 * Optional<Proyect> p = repProyect.findById(id);
 		if(p.isPresent()) {
 			return p.get();
 		}
 		return null;
+		*/
 	}
-	//public void 
+	public void agregarProyecto(Proyect p) {
+		proyectos.put((long)proyectos.size(),p);
+	}
+	public void eliminaProyecto(Long id) {
+		proyectos.remove(id);
+	}
 }
